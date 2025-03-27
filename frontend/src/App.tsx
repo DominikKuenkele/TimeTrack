4c3142b3dcd27e2f4358376b514ae3c5b8dd024f
Dominik Künkele <contact@dominik-kuenkele.de>
2025-03-27 22:28:23 +0100

    remove unneeded deps

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { CookiesProvider } from 'react-cookie';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import { AuthProvider } from './components/AuthContext';
@@ -9,21 +8,19 @@ import ProjectOverview from './components/ProjectOverview';
 
 const App: React.FC = () => {
     return (
-        <CookiesProvider>
-            <AuthProvider>
-                <Router>
-                    <div className="App">
-                        <Header />
-                        <main className="container">
-                            <Routes>
-                                <Route path="/" element={<ProjectOverview />} />
-                                <Route path="/login" element={<Login />} />
-                            </Routes>
-                        </main>
-                    </div>
-                </Router>
-            </AuthProvider>
-        </CookiesProvider>
+        <AuthProvider>
+            <Router>
+                <div className="App">
+                    <Header />
+                    <main className="container">
+                        <Routes>
+                            <Route path="/" element={<ProjectOverview />} />
+                            <Route path="/login" element={<Login />} />
+                        </Routes>
+                    </main>
+                </div>
+            </Router>
+        </AuthProvider>
     );
 };
 
