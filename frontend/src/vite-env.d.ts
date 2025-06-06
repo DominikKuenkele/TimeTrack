650a86c8e13184ee7d341b5dd166d3f658f276c4
Dominik Künkele <contact@dominik-kuenkele.de>
2025-06-06 23:31:32 +0200

    fix oauth

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
     readonly VITE_API_URL: string;
     readonly VITE_NODE_ENV: string;
+    readonly VITE_AUTH_CLIENT_ID: string;
+    readonly VITE_AUTH_SERVER_URL: string;
 }
 
 interface ImportMeta {
