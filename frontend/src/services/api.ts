8ea7939bfa2d43f86ee79ce3422665fa3b66324c
Dominik Künkele <contact@dominik-kuenkele.de>
2025-06-10 20:12:21 +0200

    fix env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import { Activity, DailyActivities, PaginatedProjects, Project } from '../types'
 import { getAccessToken } from '../utils/auth';
 import { dateToDayString } from '../utils/timeUtils';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 const NODE_ENV = import.meta.env.VITE_NODE_ENV;
 
 interface RequestOptions extends RequestInit {
